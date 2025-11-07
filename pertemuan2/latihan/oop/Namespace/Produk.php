eda2d3530468a67df500e2faaf07e9d39efabc57
Themon645 <hilmanh707@gmail.com>
2025-11-07 23:32:56 +0700

    lanjutan mengerjakan latihan OPP dan MVC

---
+++ pertemuan2/latihan/oop/Namespace/Produk.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Produk;
+
+class User {
+  public function __construct()
+  {
+    // class ini akan punya nama lengkap 'App\Product\User'
+    echo "Ini adalah class " . __CLASS__;
+  }
+}
